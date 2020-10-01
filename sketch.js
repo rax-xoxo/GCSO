df2bd99d047d98f351739d49a11c9501f0d2613f
Rashi Sharma <69389235+Rashi260@users.noreply.github.com>
2020-10-01 13:30:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,16 @@
 var car, wall; 
 var safeimg,lethalimg,averageimg,mainimg;
 var speed, weight; 
-
 function preload(){
-
  mainimg=loadImage("main.jpg");
  safeimg=loadImage("safe.jpg");
  averageimg=loadImage("average.jpg");
  lethalimg=loadImage("lethal.jpg");
-
 }
-
 function setup() {
-
   createCanvas(1400,400);
-
   speed=random(30,100);
   weight=random(700,2500);
-  
   car=createSprite(50, 200, 50,50);
   car.shapeColor="white";
   car.addImage(mainimg);
@@ -26,19 +19,14 @@ function setup() {
   car.addAnimation("average",averageimg);
   car.addAnimation("lethal",lethalimg);
   car.velocityX=speed;
-
   wall=createSprite(1340, 200, 40, 200);
   wall.shapeColor="grey";
-
 }
-
 function draw() {
   background("black");
-
   if (car.x - wall.x < wall.width/2 + car.width/2
     && wall.x - car.x < wall.width/2 + car.width/2){
     car.velocityX=0;
-
     if(((0.5*weight*speed*speed)/22500)>180){
       car.changeAnimation("lethal",lethalimg);
     }else if(((0.5*weight*speed*speed)/22500)<80){
@@ -46,8 +34,6 @@ function draw() {
     }else if(((0.5*weight*speed*speed)/22500)<180 && ((0.5*weight*speed*speed)/22500)>80){
       car.changeAnimation("average",averageimg);
     }
-
   }
-
   drawSprites();
 }
